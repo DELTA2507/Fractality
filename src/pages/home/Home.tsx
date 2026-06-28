2a13cf24b069ddff93eb0f5763afc8b9d5f1fa18
Joseph UM <joseph.ugalde.murillo@gmail.com>
2026-06-28 17:53:20 -0600

    Minor Fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
+import './Home.css'
 import Navbar from '../../components/elements/navbar/Navbar'
 import Hero from '../../components/page-sections/hero-section/Hero'
 import FractalsCatalog from '../../components/page-sections/fractals-section/FractalsCatalog'
-import './Home.css'
 
 export default function Home() {
 
